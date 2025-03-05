320d9fa13fee1df8cc2584588a387e5d03f3dc6f
Leovison <cleovison@gmail.com>
2025-03-05 19:51:37 +0800

    feat: Added Builder Configuration on the Program file to test if the Database is Working

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 using System.Reflection;
 using DatabankApi.Database;
 using DatabankApi.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpoint(Assembly.GetExecutingAssembly());
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-
+    options.UseNpgsql(builder.Configuration["ConnectionStrings:Database"]!);
 });
+
 
 var app = builder.Build();
 
