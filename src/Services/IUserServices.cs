d9fe5111c1f61efe27d35604cf63b34d3efda13e
Clark Leovison Rey <cleovison@gmail.com>
2025-06-21 15:43:27 +0800

    refactor: Moved the Folders to src

--- src/Services/IUserServices.cs
+++ src/Services/IUserServices.cs
@@ -10,6 +10,6 @@ public interface IUserServices
     Task<bool> CreateUserAsync(UserDto user, IPasswordHasher<User> passwordHasher);
     Task<IEnumerable<User>> GetAllUserAsync(CancellationToken ct);
     Task<User?> GetUserByIdAsync(Guid id);
-    Task<User?> UpdateUserAsync(User users);
+    Task<User?> UpdateUserAsync(Guid id, User users, CancellationToken ct);
     Task<bool> DeleteUserAsync(Guid id);
 }
